649916e56eb2573793704994a476b000d58431df
Konstantin <konstantin1020@gmail.com>
2021-03-11 14:48:37 -0800

    initial setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const inquirer = require('inquirer');
+const fs = require('fs')
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: '',
+      message: '',
+    },
+  ])
+  .then((response) => {
+      
+  }
+    
+  );
