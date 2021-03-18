dc1f57f1403f4f769798d895c45c4ef7f00bbf77
Konstantin <konstantin1020@gmail.com>
2021-03-18 16:12:58 -0700

    added functions to view departments and add roles

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,59 +1,166 @@
+// Dependencies
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const fs = require('fs')
 
+// Server connection
 const connection = mysql.createConnection({
   host: 'localhost',
 
-  port: 7777,
+  port: 3306,
 
-  user: 'root',
+  user: '',
 
   password: '',
-  database: '',
+  database: 'employee_db',
 });
 
-inquirer
-  .prompt([
-    {
-      name: 'action',
-      type: 'list',
-      message: 'What would you like to do?',
-      choices: [
-        'Find songs by artist',
-        'Find all artists who appear more than once',
-        'Find data within a specific range',
-        'Search for a specific song',
-        'exit',
-      ],
-    },
-  ])
-  .then((response) => {
-      switch (response.action) {
-        case "view departments": 
-          viewDepartment();
+// Main function to navigate user in CLI
+const allQuestions = () => {
+  inquirer
+    .prompt(
+      {
+        name: "questions",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View Departments",
+          "View Employees",
+          "View Roles",
+          "Add Department",
+          "Add Employee",
+          "Add Role",
+          "Update Department",
+          "Update Employee",
+          "Update Role",
+          "Exit",
+        ],
+      },
+    )
+    .then((response) => {
+      switch (response.questions) {
+        case "View Departments":
+          viewDepartments();
           break;
-      
+
+        case "View Employees":
+          viewEmployees();
+          break;
+
+        case "View Roles":
+          viewRoles();
+          break;
+
+        case "Add Department":
+          addDepartment();
+          break;
+
+        case "Add Employee":
+          addEmployee();
+          break;
+
+        case "Add Role":
+          addRole();
+          break;
+
+        case "Update Employee":
+          updateEmployee();
+          break;
+
+        case "Exit":
+          connection.end();
+          break;
+
         default:
+          console.log(`Invalid action: ${response.action}`);
           break;
       }
-  }
-    
-  );
+    });
+};
 
-  function viewDepartment() {
-    console.log()
-  }
-  
-const afterConnection = () => {
-  connection.query('SELECT * FROM', (err, res) => {
-    if (err) throw err;
-    console.log(res);
-    connection.end();
-  })
+const viewDepartments = () => {
+  const query = "SELECT * FROM department";
+  connection.query(query, (err, res) => {
+    res.forEach(({ name }) => console.log(name));
+    allQuestions();
+  });
+};
+
+const addDepartment = () => {
+  inquirer
+    .prompt(
+      {
+        name: "newDepartment",
+        type: "input",
+        message: "Please add a new department",
+      }
+    )
+    .then((answer) => {
+      const query = "INSERT INTO department SET ?"
+      connection.query(query,
+        {
+          name: answer.newDepartment
+        },
+        (err, res) => {
+          if (err) throw err;
+        })
+      allQuestions();
+    })
 }
-connection.connect((err) => {
-  if (err) throw err;
-  console.log(`connected as id ${connection.threadId}`);
-  connection.end();
-});
+
+const addRole = () => {
+  const query = "SELECT * FROM department";
+  // const query = "SELECT id, name AS title FROM department"
+  connection.query(query, (err, res) => {
+    inquirer
+      .prompt([
+        {
+          name: "newRole",
+          type: "input",
+          message: "What's the title of the this new role?",
+        },
+        {
+          name: "newSalary",
+          type: "input",
+          message: "What's the salary for this role?",
+        },
+        {
+          name: "roleDepartment",
+          type: "list",
+          message: "What department does this role belong to",
+          choices: res
+        }
+      ])
+      .then((answer) => {
+        let match = res.find((dept) => {
+          return dept.name === answer.roleDepartment
+        })
+        console.log(match)
+        const query = "INSERT INTO role SET ?"
+        connection.query(query,
+          {
+            title: answer.newRole,
+            salary: answer.newSalary,
+            department_id: match.id
+          },
+          (err, res) => {
+            if (err) throw err;
+          })
+        allQuestions();
+      })
+  })
+};
+// const afterConnection = () => {
+//   connection.query('SELECT * FROM', (err, res) => {
+//     if (err) throw err;
+//     console.log(res);
+//     connection.end();
+//   });
+// };
+// connection.connect((err) => {
+//   if (err) throw err;
+//   console.log(`connected as id ${connection.threadId}`);
+//   connection.end();
+// });
+
+allQuestions();
